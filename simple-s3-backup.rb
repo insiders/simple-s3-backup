ee7778973ebfd9b0974a64500a00eeb4f71bc39e
Martijn Heemels <martijn@insiders.nl>
2014-09-03 12:07:35 +0200

    Allow skipping of deletion phase. when SKIP_DELETE is to true the script only writes new backups and creates new buckets. [SYSPRJ-36]
    No attempts are made to delete data from S3. Set this to true when the credentials you provide don't have the rights to perform deletions. This is a handy way to prevent disaster should someone malicious gain access to them. With correctly restricted IAM permissions they won't be able to delete existing backups. You'll need to use a separate server and credentials to purge old backups.

--- simple-s3-backup.rb
+++ simple-s3-backup.rb
@@ -108,8 +108,10 @@ end
 # Remove tmp directory
 FileUtils.remove_dir full_tmp_path
 
-# Now, clean up unwanted archives
-cutoff_date = Time.now.utc.to_i - (DAYS_OF_ARCHIVES * 86400)
-bucket.objects.select{ |o| o.last_modified.to_i < cutoff_date }.each do |f|
-  S3Object.delete(f.key, S3_BUCKET)
+# Now, clean up unwanted archives, if allowed
+unless SKIP_DELETE
+  cutoff_date = Time.now.utc.to_i - (DAYS_OF_ARCHIVES * 86400)
+  bucket.objects.select{ |o| o.last_modified.to_i < cutoff_date }.each do |f|
+    S3Object.delete(f.key, S3_BUCKET)
+  end
 end
